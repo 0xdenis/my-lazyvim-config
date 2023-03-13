eff5eca16ec7b54f2ab750431656538b9b02b49f
Denis Huber <denishuber88@gmail.com>
2023-03-13 11:32:03 +0100

    replace tab keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,19 @@ map("n", "<leader>ww", "<cmd>w<cr>", { desc = "Save Current Buffer" })
 map("n", "<leader>wa", "<cmd>wa<cr>", { desc = "Save All" })
 map("n", "<leader>qq", "<cmd>q<cr>", { desc = "Close Window" })
 map("n", "<leader>qa", "<cmd>qa<cr>", { desc = "Close All" })
+
+-- windows
+map("n", "<leader>wc", "<C-W>c", { desc = "Delete window" })
+
+-- tabs
+require("which-key").register({
+  ["<leader>t"] = { name = "tabs" },
+  ["<leader><tab>"] = "which_key_ignore",
+})
+map("n", "<leader>tl", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader>tf", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader>tt", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader>t]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader>t[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader>td", "<cmd>tabclose<cr>", { desc = "Close Tab" })
